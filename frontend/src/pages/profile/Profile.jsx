4c2e2031da49c228518896b64a84e516851ca0ea
Shikaruuuu <horuzou17@gmail.com>
2024-09-09 01:20:17 +0900

    adjust profile to mobile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState, useContext } from 'react';
-import Topbar from '../../components/topbar/Topbar';
-import Sidebar from '../../components/sidebar/Sidebar';
-import Timeline from '../../components/timeline/Timeline';
-import Rightbar from '../../components/rightbar/Rightbar';
+import React, { useEffect, useState, useContext } from "react";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Timeline from "../../components/timeline/Timeline";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./Profile.css";
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { AuthContext } from '../../state/AuthContext';
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../state/AuthContext";
+import Hamburger from "../../components/hamburger/Hamburger";
+import TopbarMobile from "../../components/topbarMobile/TopbarMobile";
 
 export default function Profile() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
-  const { userId } = useParams();  // 現在のプロフィールページのユーザーID
-  const { user: currentUser } = useContext(AuthContext);  // ログインしているユーザー
+  const { userId } = useParams(); // 現在のプロフィールページのユーザーID
+  const { user: currentUser } = useContext(AuthContext); // ログインしているユーザー
   const [isFollowed, setIsFollowed] = useState(false);
 
   useEffect(() => {
@@ -35,7 +37,9 @@ export default function Profile() {
   const handleFollow = async () => {
     try {
       if (isFollowed) {
-        await axios.put(`/users/${userId}/unfollow`, { userId: currentUser.id });
+        await axios.put(`/users/${userId}/unfollow`, {
+          userId: currentUser.id,
+        });
       } else {
         await axios.put(`/users/${userId}/follow`, { userId: currentUser.id });
       }
@@ -45,23 +49,54 @@ export default function Profile() {
     }
   };
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // ウィンドウサイズが変更された時にサイズを更新する
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // コンポーネントがアンマウントされた際にイベントリスナーを削除
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <Topbar />
+      {windowWidth >= 1036 ? <Topbar /> : <TopbarMobile />}
       <div className="profile">
-        <Sidebar />
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
               {user.coverPicture ? (
-                <img src={PUBLIC_FOLDER + user.coverPicture} alt="" className="profileCoverImg" />
+                <img
+                  src={PUBLIC_FOLDER + user.coverPicture}
+                  alt=""
+                  className="profileCoverImg"
+                />
               ) : (
-                <img src={PUBLIC_FOLDER + "/post/3.jpeg"} alt="" className="profileCoverImg" />
+                <img
+                  src={PUBLIC_FOLDER + "/post/3.jpeg"}
+                  alt=""
+                  className="profileCoverImg"
+                />
               )}
               {user.profilePicture ? (
-                <img src={PUBLIC_FOLDER + user.profilePicture} alt="" className="profileUserImg" />
+                <img
+                  src={PUBLIC_FOLDER + user.profilePicture}
+                  alt=""
+                  className="profileUserImg"
+                />
               ) : (
-                <img src={PUBLIC_FOLDER + "/person/noAvatar.png"} alt="" className="profileUserImg" />
+                <img
+                  src={PUBLIC_FOLDER + "/person/noAvatar.png"}
+                  alt=""
+                  className="profileUserImg"
+                />
               )}
             </div>
             <div className="profileInfo">
@@ -69,7 +104,7 @@ export default function Profile() {
               <span className="profileInfoDesc">{user.desc}</span>
               {userId !== String(currentUser.id) && (
                 <button className="followButton" onClick={handleFollow}>
-                  {isFollowed ? 'フォロー解除' : 'フォロー'}
+                  {isFollowed ? "フォロー解除" : "フォロー"}
                 </button>
               )}
             </div>
@@ -78,7 +113,7 @@ export default function Profile() {
             <Timeline isProfile={true} profileUserId={userId} />
           </div>
         </div>
-        <Rightbar user={user} />
+        {windowWidth >= 1036 ? <Rightbar user={user} /> : <Hamburger />}
       </div>
     </>
   );
