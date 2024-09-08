2b5ff06a69690b41d86af9418bd4a62bd73f13cd
Shikaruuuu <horuzou17@gmail.com>
2024-09-09 01:15:49 +0900

    adjust request layout to mobile

--- frontend/src/pages/requestPost/RequestPost.jsx
+++ frontend/src/pages/requestPost/RequestPost.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Topbar from '../../components/topbar/Topbar';
-import Sidebar from '../../components/sidebar/Sidebar';
-import Request from '../../components/request/Request';
-import Rightbar from '../../components/rightbar/Rightbar';
+import React, { useEffect, useState } from "react";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Request from "../../components/request/Request";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./RequestPost.css";
+import TopbarMobile from "../../components/topbarMobile/TopbarMobile";
+import Hamburger from "../../components/hamburger/Hamburger";
 
 export default function RequestPost() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // ウィンドウサイズが変更された時にサイズを更新する
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // コンポーネントがアンマウントされた際にイベントリスナーを削除
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <Topbar />
+      {windowWidth >= 1036 ? <Topbar /> : <TopbarMobile />}
       <div className="requestPostContainer">
-      <Sidebar />
-      <Request />
-      <Rightbar />
+        <Request />
+        {windowWidth >= 1036 ? <Rightbar /> : <Hamburger />}
       </div>
     </>
   );
